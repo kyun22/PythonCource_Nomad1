43c188dc19b04a8dfce08c24ceb06d95c7565736
shlee <seokhyun90@gmail.com>
2021-04-15 23:27:15 +0900

    20210415 Study python, requests, BeautifulSoup

--- indeed.py
+++ indeed.py
@@ -1,2 +1,23 @@
-import urllib
+import requests
+from bs4 import BeautifulSoup
 
+LIMIT = 50
+URL = "https://kr.indeed.com/jobs?q=python&limit=50&radius=25"
+
+def extract_indeed_pages():
+    req = requests.get(URL)
+    soup = BeautifulSoup(req.text, 'html.parser')
+    pagination = soup.find("div", {"class": "pagination"})
+    links = pagination.find_all('a')
+    pages = []
+    for link in links[:-1]:
+        # pages.append(link.find("span").string)
+        pages.append(int(link.string))
+    max_page = pages[-1]
+    return max_page
+
+
+def extract_indeed_jobs(last_page):
+    for page in range(last_page):
+        result = requests.get(f"{URL}&start={page * LIMIT}")
+        print(result.status_code)
--- main.py
+++ main.py
@@ -1,68 +1,17 @@
-# This is a sample Python script.
 
-# Press ⌃R to execute it or replace it with your code.
-# Press Double ⇧ to search everywhere for classes, files, tool windows, actions, and settings.
+from indeed import extract_indeed_pages, extract_indeed_jobs
 
 
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press ⌘F8 to toggle the breakpoint.
 
-def plus(a, b):
-    if type(a) is not int or type(b) is not int:
-        return a + b
-    else:
-        return None
-
-def minus(a, b):
-    if type(a) is not int or type(b) is not int:
-        return a - b
-    else:
-        return None
-
-def times(a, b):
-    if type(a) is not int or type(b) is not int:
-        return a * b
-    else:
-        return None
-
-def division(a, b):
-    if type(a) is not int or type(b) is not int:
-        return a / b
-    else:
-        return None
-
-def power(a, b):
-    if type(a) is not int or type(b) is not int:
-        return a ** b
-    else:
-        return None
-
-def negation(a):
-    if type(a) is not int:
-        return -a
-    else:
-        return None
-def reminder(a, b):
-    if type(a) is not int or type(b) is not int:
-        return a % b
-    else:
-        return None
 
 # Press the green button in the gutter to run the script.
 if __name__ == '__main__':
-    print_hi('PyCharm')
 
-    a = "10"
-    b = 27.2
+    last_indeed_page = extract_indeed_pages()
+    print(extract_indeed_jobs(last_indeed_page))
 
-    print(plus(a, b))
-    print(minus(a, b))
-    print(times(a, b))
-    print(division(a, b))
-    print(power(a, b))
-    print(negation(a))
-    print(reminder(a, b))
+
+
 
 
 
